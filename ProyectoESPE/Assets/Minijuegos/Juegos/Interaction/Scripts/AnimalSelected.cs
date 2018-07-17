03eb80a5d5f1fdc440940632321dfc139b28195d
Marco Macias <mamacias3@espe.edu.ec>
2018-07-16 23:24:41 -0500

    añadir carpetas dañadas

---
+++ ProyectoESPE/Assets/Minijuegos/Juegos/Interaction/Scripts/AnimalSelected.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AnimalSelected : MonoBehaviour {
+
+	public GameObject kinectcontrol;
+	public Text texto;
+	string activeobjname="";
+	GrabDropScript sc;
+	GenerarAnimal gen;
+	bool ok=false;
+	int puntos=-1;
+
+
+
+	// Use this for initialization
+	void Start () {
+		sc=kinectcontrol.GetComponent<GrabDropScript>();
+		gen=kinectcontrol.GetComponent<GenerarAnimal>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		activeobjname=sc.objname;
+		if(activeobjname.Equals(gen.nombreactivo) ){
+			puntos++;
+			texto.text="Puntos: "+puntos;
+			print("nombre activo: "+activeobjname+" puntos: "+puntos);
+			sc.objname="";
+			gen.CambiarAnimal();
+		}else if(!activeobjname.Equals(gen.nombreactivo) && !activeobjname.Equals("")){
+			gen.CambiarAnimal();
+			sc.objname="";
+		}
+	}
+}
---
+++ ProyectoESPE/Assets/Minijuegos/Juegos/Interaction/Scripts/MoveActor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveActor : MonoBehaviour {
+
+	public GameObject loc1, loc2;
+	public GameObject actorCam;
+
+	public void MoveToLocation1(){
+		Vector3 location=loc1.transform.position;
+		location.x=location.z-50;
+		actorCam.transform.Translate(location);
+	}
+
+	public void MoveToLocation2(){
+		Vector3 location=loc2.transform.position;
+		location.x=location.z-50;
+		actorCam.transform.Translate(location);
+	}
+}
